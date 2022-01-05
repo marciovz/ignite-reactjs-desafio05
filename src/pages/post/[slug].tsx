a32a9d6d30b5d0cc6869b78d7d15d434267d5e9c
marciovz <marciovz@yahoo.com.br>
2022-01-05 08:31:44 -0300

    implementado os metodos getStaticPath e getStaticProps do [slug].tsx e correçoes no ccs do header

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,6 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { FiClock, FiUser, FiCalendar } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
+import Header from '../../components/Header';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
@@ -26,20 +29,83 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post() {
+  // TODO
+  return (
+   <>
+      <Header />
+      <img src="" alt="" />
+      <main>
+        <article>
+          <h1>Titulo do post</h1>
+            <div className={styles.info}>
+              <div>
+                <FiCalendar />
+                <time>10 Jan 2022</time>
+              </div>
+              <div>
+                <FiUser />
+                <span>John Due</span>
+              </div>
+              <div>
+                <FiClock />
+                <time>4 min</time>
+              </div>
+            </div>
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+        </article>
+      </main>
+   </>
+  )
+}
 
-//   // TODO
-// };
+// Get two posts and create as static pages
+export const getStaticPaths: GetStaticPaths = async() => {
+  const prismic = getPrismicClient();
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ],{
+    fetch: ['post.title'],
+    pageSize: 2,
+  });
 
-//   // TODO
-// };
+  const paths = posts.results.map(post => (
+    {
+      params: { slug: post.uid }
+    }
+  ));
+
+  return {
+    paths,
+    fallback: true  
+  }
+};
+
+// Get post by slug 
+export const getStaticProps: GetStaticProps = async context => {
+  const { slug } = context.params;
+  
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getByUID('post', String(slug), {});
+  
+  const post = {
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content,
+    }
+  }
+  
+  return {
+    props: {
+      post
+    },
+    revalidate: 60 * 30   // 30 minutos
+  }
+}
